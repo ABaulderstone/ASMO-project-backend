58af7a5028d36c0c7081d5157e834aeb43d5a313
sjwconway <sjwconway@hotmail.com>
2019-07-10 16:28:34 +1000

    added Staff API routes and fix Staff Schema

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const AuthRoutes = require("./auth_routes");
-const RestaurantRoutes = require("./restaurant_routes")
+const RestaurantRoutes = require("./restaurant_routes");
+const StaffRoutes = require("./staff_routes");
 // const BookmarkRoutes = require("./bookmark_routes");
 const passport = require("passport");
 
 router.get("/", (req, res) => res.send("Welcome to ASMO"));
 router.use("/auth", AuthRoutes);
 router.use("/restaurant", passport.authenticate("jwt", {session: false}), RestaurantRoutes);
+router.use("/staff", passport.authenticate("jwt", {session: false}), StaffRoutes);
+
 // router.use("/bookmarks",passport.authenticate("jwt", {session: false}), BookmarkRoutes)
 
 module.exports = router;
