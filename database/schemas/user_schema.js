0ca57c84d85c367d8586871d4d844dd1a6b1dfe0
sjwconway <sjwconway@hotmail.com>
2019-07-10 11:06:46 +1000

    blank

--- database/schemas/user_schema.js
+++ database/schemas/user_schema.js
@@ -7,6 +7,7 @@ const { Schema } = require("mongoose");
 const passportLocalMongoose = require('passport-local-mongoose');
 
 const UserSchema = new Schema({
+    
 });
 
 UserSchema.plugin(passportLocalMongoose, { usernameField: 'email' });
