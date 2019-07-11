27739878e0278612813ac0fdde8c65521c352293
Alex Baulderstone <alex.baulderstone@gmail.com>
2019-07-11 10:29:35 +1000

    added validation error handling

--- middleware/error_handler_middleware.js
+++ middleware/error_handler_middleware.js
@@ -1,7 +1,14 @@
 module.exports = function(err, req, res, next) {
+    console.log(err);
     if (err && err.name === "HTTPError") {
-        return res.status(err.statusCode).send(err.message);
+        return res.status(err.statusCode).json({_error:err.message});
+    } else if (err && err.name==="ValidationError") {
+        console.log(err.details[0].message)
+        return res.status(422).json({ _error: err.details[0].message});
+        
     }
+
+
 
     return next(err);
 }
