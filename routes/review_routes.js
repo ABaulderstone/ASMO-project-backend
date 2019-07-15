5fa91b95aa7655e21acac2ba0668e4595b38cd9a
Alex Baulderstone <alex.baulderstone@gmail.com>
2019-07-15 14:00:53 +1000

    tidied up some joi validation

--- routes/review_routes.js
+++ routes/review_routes.js
@@ -3,16 +3,16 @@ const { celebrate, Joi } = require("celebrate");
 const router = express.Router();
 const ReviewController = require("./../controllers/review_controller");
 
-router.get("/", celebrate({
+router.get("/", ReviewController.index);
+
+router.post("/", celebrate({
     body: {
-        foodRating: Joi.string().required(),
-        serviceRating: Joi.number().required(),
-        comment: Joi.string()
+        foodRating: Joi.number().integer().min(1).max(5).required(),
+        serviceRating: Joi.number().integer().min(1).max(5).required(),
+        comment: Joi.string().max(250)
 
     }
-}), ReviewController.index);
-
-router.post("/", 
+}), 
  ReviewController.create)
 
 router.get("/:id", ReviewController.show);
