ac8dbf82ed4883a715646fc9ea4dd4b7c0b78196
obpumanee <obpumanee@gmail.com>
2019-07-15 15:27:17 +1000

    sedgrid automate emial

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,16 @@ const AuthRoutes = require("./auth_routes");
 const CustomerRoutes = require("./customer_routes");
 const StaffRoutes = require("./staff_routes");
 const passport = require("passport");
+const MailerService = require("./../services/mail_service");
 
 router.get("/", (req, res) => res.send("Welcome to ASMO"));
 router.use("/auth", AuthRoutes);
 router.use("/customer", passport.authenticate("jwt", { session: false }), CustomerRoutes);
 router.use("/staff", passport.authenticate("jwt", { session: false }), StaffRoutes);
-
+router.get("/test", (req, res) => {
+    MailerService.sendConfirmMail()
+    res.send("Welcome to ...")
+})
 
 
 module.exports = router;
