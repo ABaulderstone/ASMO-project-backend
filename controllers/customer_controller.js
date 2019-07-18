a3270af64b3e209205765880abc892d10bd7f9ee
Alex Baulderstone <alex.baulderstone@gmail.com>
2019-07-18 11:29:35 +1000

    fixing merge conflicts

--- controllers/customer_controller.js
+++ controllers/customer_controller.js
@@ -6,7 +6,7 @@ async function create(req, res, next) {
   } catch (err) {
     next(err);
   }
-   return res.json(req.user.customers);
+  return res.json(req.user.customers);
 }
 
 function index(req, res) {
@@ -16,11 +16,11 @@ function index(req, res) {
 async function update(req, res, next) {
   const { id } = req.params;
   const customer = req.user.customers.id(id);
-  const {name, phone, email} = req.body;
+  const { name, phone, email } = req.body;
   customer.name = name;
   customer.phone = phone;
   customer.email = email;
- 
+
   try {
     await req.user.save();
   } catch (err) {
@@ -33,24 +33,22 @@ function show(req, res) {
   const { id } = req.params;
   const customer = req.user.customers.id(id);
   if (customer) {
-  return res.json(customer);
+    return res.json(customer);
   }
-  return res.status(404).json({_error: "Customer not found"});
+  return res.status(404).json({ _error: "Customer not found" });
 }
 
- async function destroy(req, res, next) {
-    const { id } = req.params;
-    const customer = req.user.customers.id(id);
-    customer.remove();
-    try {
-        await req.user.save();
-      } catch (err) {
-        next(err);
-      }
-      
-      return res.json(req.user.customers);
+async function destroy(req, res, next) {
+  const { id } = req.params;
+  const customer = req.user.customers.id(id);
+  customer.remove();
+  try {
+    await req.user.save();
+  } catch (err) {
+    next(err);
+  }
 
-
+  return res.json(req.user.customers);
 }
 
 module.exports = {
