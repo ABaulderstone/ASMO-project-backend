e72bd0700a12a2d2a8e4e2ce1f53e24299350f83
Alex Baulderstone <alex.baulderstone@gmail.com>
2019-07-18 11:23:46 +1000

    fixed customer controller broken by bad github merge

--- controllers/customer_controller.js
+++ controllers/customer_controller.js
@@ -1,44 +1,26 @@
-const sg = require("./../services/mail_service");
-
 async function create(req, res, next) {
   req.user.customers.push(req.body);
 
-  console.log(req.body);
   try {
     await req.user.save();
   } catch (err) {
     next(err);
   }
+   return res.json(req.user.customers);
+}
+
+function index(req, res) {
   return res.json(req.user.customers);
-}
-return res.json(customer);
-
-
-function show(req, res) {
-  const { id } = req.params;
-  const customer = req.user.customers.id(id);
-  return res.json(customer);
-}
-
-async function destroy(req, res) {
-  const { id } = req.params;
-  const customer = req.user.customers.id(id);
-  customer.remove();
-  try {
-    await req.user.save();
-  } catch (err) {
-    next(err);
-  }
 }
 
 async function update(req, res, next) {
   const { id } = req.params;
   const customer = req.user.customers.id(id);
-  const { name, phone, email } = req.body;
+  const {name, phone, email} = req.body;
   customer.name = name;
   customer.phone = phone;
   customer.email = email;
-
+ 
   try {
     await req.user.save();
   } catch (err) {
@@ -51,22 +33,22 @@ function show(req, res) {
   const { id } = req.params;
   const customer = req.user.customers.id(id);
   if (customer) {
-    return res.json(customer);
+  return res.json(customer);
   }
-  return res.status(404).json({ _error: "Customer not found" });
+  return res.status(404).json({_error: "Customer not found"});
 }
 
-async function destroy(req, res, next) {
-  const { id } = req.params;
-  const customer = req.user.customers.id(id);
-  customer.remove();
-  try {
-    await req.user.save();
-  } catch (err) {
-    next(err);
-  }
-
-  return res.json(req.user.customers);
+ async function destroy(req, res, next) {
+    const { id } = req.params;
+    const customer = req.user.customers.id(id);
+    customer.remove();
+    try {
+        await req.user.save();
+      } catch (err) {
+        next(err);
+      }
+      
+      return res.json(req.user.customers);
 
 
 }
