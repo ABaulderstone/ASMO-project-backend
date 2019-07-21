01e90a9898935a0f37ec6c0ad999b7277b3e4aaf
Alex Baulderstone <alex.baulderstone@gmail.com>
2019-07-21 18:41:53 +1000

    added role to staff schema

--- database/schemas/staff_schema.js
+++ database/schemas/staff_schema.js
@@ -5,6 +5,11 @@ const StaffSchema = new Schema({
         type: String, 
         required: true
     },
+    role: {
+        type: String,
+        required: true,
+        default: "Off"
+    },
     
     avatar: {
         type: String,
--- middleware/error_handler_middleware.js
+++ middleware/error_handler_middleware.js
@@ -1,17 +1,15 @@
 module.exports = function(err, req, res, next) {
-    console.log(err);
-    if (err && err.name === "HTTPError") {
-        return res.status(err.statusCode).json({_error: err.message});
-    } else if (err && err.name==="ValidationError") {
-        
-         if(err.details[0].type ==="string.regex.base") {
-            return res.status(422).json({ _error: "Not a valid Australian phone number"});
-         }
-        return res.status(422).json({ _error: err.details[0].message});
-        
+  console.log(err);
+  if (err && err.name === "HTTPError") {
+    return res.status(err.statusCode).json({ _error: err.message });
+  } else if (err && err.name === "ValidationError") {
+    if (err.details[0].type === "string.regex.base") {
+      return res
+        .status(422)
+        .json({ _error: "Not a valid Australian phone number" });
     }
+    return res.status(422).json({ _error: err.details[0].message });
+  }
 
-
-
-    return next(err);
-}
+  return next(err);
+};
