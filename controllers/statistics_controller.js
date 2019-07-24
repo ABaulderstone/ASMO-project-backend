99daa4fd8ab283a577b9a562704212967ca1bfc3
Alex Baulderstone <alex.baulderstone@gmail.com>
2019-07-24 11:22:11 +1000

    controller returns documents for user

--- controllers/statistics_controller.js
+++ controllers/statistics_controller.js
@@ -1,4 +1,14 @@
-function index (req, res,next){
+const StatisticsModel = require("./../database/models/statistics_model");
+
+async function index (req, res,next){
+    const {_id} = req.user;
+    StatisticsModel.find({owner: _id}, (err, document) => {
+        if (err) {
+            next(err)
+        }
+        res.send(document);
+    }
+    )
 
 }
 
